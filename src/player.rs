0afd4bd679456f8f1fc3a9867481777d0c2f98bb
Enn3Developer <arturpeshko39@gmail.com>
2022-05-08 15:26:48 +0200

    Updated the bare-bone logic of the players

--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,24 @@
 use crate::board::{Board, BoardConfig};
+use crate::math::Vec2;
 
-struct Player {
+trait Player {
+    fn name(&self) -> &str;
+    fn board(&self) -> &Board;
+    fn points(&self) -> u64;
+
+    fn hit(&self, enemy: &Board) -> Vec2;
+
+    fn add_points(&mut self);
+    fn got_hit(&self, point: Vec2) -> bool;
+}
+
+struct HumanPlayer {
     name: String,
     board: Board,
     points: u64,
 }
 
-impl Player {
+impl HumanPlayer {
     pub fn new(name: String, config: BoardConfig) -> Self {
         unimplemented!()
     }
